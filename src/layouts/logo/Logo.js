5318b638b786a86ea3cf3807d24a992d58925f40
firasbelhiba <firas.belhiba@esprit.tn>
2022-03-30 21:25:46 +0100

    updating logo

--- src/layouts/logo/Logo.js
+++ src/layouts/logo/Logo.js
@@ -1,4 +1,4 @@
-import LogoDark from "../../assets/images/logos/amplelogo.svg";
+import LogoDark from "../../assets/images/logos/logo.png";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,7 +6,7 @@ const Logo = () => {
   return (
     <Link href="/">
       <a>
-        <Image src={LogoDark} alt="logo" />
+        <Image src={LogoDark} alt="logo" width="150" height="150"/>
       </a>
     </Link>
   );
